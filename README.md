0826e165215c014a167df52ab46f08654c561269
Cesar Cara <cesar.cara@protonmail.com>
2023-05-02 10:11:04 -0300

    parsing flags and available commands

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ Polyglot Todo is an programming language experiment. No rocket science, only the
 - [ ] Test the add a Todo item.
 - [ ] Persist the data in a file.
 - [ ] Have a way to push the data to an external repository.
+
+## Commands
+
+### Tasks
+- `todo -l`: Lists all tasks in the current board
+- `todo -a the task name`: Adds a task
+- `todo -d 3`: Marks a task as done
+- `todo -D 3`: Deletes a task
+- `todo -m 1`: Moves the task the given position
+- `todo -u 1 the new name of the task`: Updates a task in place
--- go/cmd/cli/main.go
+++ go/cmd/cli/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log"
 )
@@ -11,24 +12,52 @@ func main() {
 	}
 }
 
-const (
-	CmdTask = "task"
-)
-
 func run() error {
-	flag.Parse()
+	var f flag.FlagSet
+	f.Bool("l", false, "Lists all current tasks.")
+	f.Bool("a", false, "Adds a new task to the board.")
+	f.Int("d", 0, "Marks a task as done.")
+	f.Int("D", 0, "Deletes a task.")
+	f.Int("m", 0, "Moves a task to the given poisition.")
+	f.Int("u", 0, "Updates the title of the given task.")
+	f.Parse(flag.Args())
 
-	// Branch to the requested command.
-	switch flag.Arg(0) {
-	case CmdTask:
-		c := NewCommandTask(flag.Args())
+	if err := validateFlags(f); err != nil {
+		return err
+	}
 
-	default:
-		flag.Usage()
+	flags := newFlags(f)
+
+	switch {
+	case flags.list:
+		// call some list function
 	}
 
 	return nil
 }
 
-// Create a task.
-// ./todo task New task name
+func validateFlags(f flag.FlagSet) error {
+	if f.NFlag() > 1 {
+		return errors.New("cannot use multiple flags")
+	}
+
+	return nil
+}
+
+type flags struct {
+	list   bool
+	add    bool
+	done   int
+	delete int
+	move   int
+	update int
+}
+
+func newFlags(f flag.FlagSet) flags {
+	// If no flag was set consider as a list command.
+	if f.NFlag() == 0 {
+		return flags{list: true}
+	}
+
+	return flags{}
+}
